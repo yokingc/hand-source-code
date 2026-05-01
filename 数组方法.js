3e549d2821c53624767a7d54fe8ee7d9867b8889
费晋 <1484812364@qq.com>
2026-05-01 12:03:00 +0800

    feat: 补全数组方法手写

--- 数组方法.js
+++ 数组方法.js
@@ -56,3 +56,49 @@ function uniqueWithMap(arr,key) {
     const map = new Map()
     return arr.filter(item => !map.has(item[key]) && map.set(item[key], true))
 }
+
+// 实现数组的map reduce filter
+
+// ★ 重点：map 返回新数组，不修改原数组；callback 接收 (item, index, array) 三个参数
+// ★ 加分点：用 Array.prototype.xxx = function 挂载到原型，this 就是调用的数组
+Array.prototype.myMap = function(callback, thisArg) {
+    // 加分点：处理稀疏数组，跳过空槽（原生 map 也会跳过）
+    if (typeof callback !== 'function') throw new TypeError('callback must be a function')
+    const result = new Array(this.length)
+    for (let i = 0; i < this.length; i++) {
+        if (i in this) { // 加分点：in 操作符可以检测稀疏数组的空槽
+            result[i] = callback.call(thisArg, this[i], i, this)
+        }
+    }
+    return result
+}
+
+// ★ 重点：filter 返回新数组，只保留 callback 返回 truthy 的元素
+Array.prototype.myFilter = function(callback, thisArg) {
+    if (typeof callback !== 'function') throw new TypeError('callback must be a function')
+    const result = []
+    for (let i = 0; i < this.length; i++) {
+        if (i in this && callback.call(thisArg, this[i], i, this)) {
+            result.push(this[i])
+        }
+    }
+    return result
+}
+
+// ★ 重点：reduce 核心是累加器 acc；initialValue 不传时，acc 默认是第一个元素，遍历从第二个开始
+// ★ 加分点：空数组且没有 initialValue 时，原生 reduce 会抛 TypeError，要模拟这个行为
+Array.prototype.myReduce = function(callback, initialValue) {
+    if (typeof callback !== 'function') throw new TypeError('callback must be a function')
+    const hasInitial = arguments.length >= 2
+    if (this.length === 0 && !hasInitial) throw new TypeError('Reduce of empty array with no initial value')
+
+    let acc = hasInitial ? initialValue : this[0]
+    let startIndex = hasInitial ? 0 : 1
+
+    for (let i = startIndex; i < this.length; i++) {
+        if (i in this) {
+            acc = callback(acc, this[i], i, this)
+        }
+    }
+    return acc
+}
